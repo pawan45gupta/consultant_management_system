4228dac4c2ff41af8700701d05bfb49e9b9fbd44
Pawan Gupta <pawangupta@Pawan-G-MacBook-Pro.local>
2021-10-21 12:18:13 +0530

    Add consultant

--- app/ReactApp/src/Components/AddConsultantModal.jsx
+++ app/ReactApp/src/Components/AddConsultantModal.jsx
@@ -1,39 +1,67 @@
-import React from "react";
-import { Button, Modal, InputGroup, FormControl } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  InputGroup,
+  Form,
+  Col,
+} from "react-bootstrap";
 
-const AddConsultantModal = ({ show, handleClose, handleAdd, rowData }) => (
-  <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Add Consultant</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      {rowData &&
-        rowData.length > 0 &&
-        Object.keys(rowData[0]).map(
-          (col) =>
-            col !== "id" && (
-              <div>
-                <InputGroup className="mb-3">
-                  <FormControl
-                    id={col}
-                    placeholder={`Enter ${col}`}
-                    aria-label={col}
-                    aria-describedby={col}
-                  />
-                </InputGroup>
-              </div>
-            )
-        )}
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Close
-      </Button>
-      <Button variant="primary" onClick={handleAdd}>
-        Add
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+const AddConsultantModal = ({ show, handleClose, handleAdd, rowData }) => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    form.checkValidity() && handleAdd();
+    setValidated(true);
+  };
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Consultant</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          {rowData &&
+            rowData.length > 0 &&
+            Object.keys(rowData[0]).map(
+              (col) =>
+                col !== "id" && (
+                  <div>
+                    <Form.Group
+                      as={Col}
+                      md="12"
+                      controlId="validationCustomUsername"
+                    >
+                      <Form.Label>{col}</Form.Label>
+                      <InputGroup hasValidation>
+                        <Form.Control
+                          id={col}
+                          type="text"
+                          placeholder={`Enter ${col}`}
+                          required
+                          defaultValue={"test"}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          {`Please enter ${col}`}
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                  </div>
+                )
+            )}
+          <Button className="m-2" type="submit">
+            Add
+          </Button>
+          <Button className="m-2" variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
 
 export default AddConsultantModal;
